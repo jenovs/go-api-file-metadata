cac8a7aefd30f9e9066277dfbbf9b82f78042fd6
Viktors Jenovs <jviktors@gmail.com>
2017-04-29 19:28:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# File Metadata Microservice
+
+## Objective
+
+Build a full stack app that is functionally similar to this: https://aryanj-file-size.herokuapp.com/.
+
+## Requirements
+
+* User Story: I can submit a FormData object that includes a file upload.
+
+* User Story: When I submit something, I will receive the file size in bytes within the JSON response
---
+++ server.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"html/template"
+	"io"
+	"log"
+	"os"
+)
+
+type FileData struct {
+  Name string `json:"filename"`
+  Size int64  `json:"size"`
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("upload.html")
+		t.Execute(w, "")
+	} else {
+		r.ParseMultipartForm(1 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+
+		if err != nil {
+			fmt.Println("Parse error: ", err)
+			return
+		}
+
+		defer file.Close()
+		f, err := os.Create("./test/" + handler.Filename)
+
+		if err != nil {
+			fmt.Println("File create error: ", err)
+			return
+		}
+
+		io.Copy(f, file)
+		size, err := f.Stat()
+		fmt.Println(size.Size())
+
+		fileData := FileData{handler.Filename, size.Size()}
+		res, err := json.Marshal(fileData)
+
+    if err != nil {
+      fmt.Println("JSON marshal error: ", err)
+      return
+    }
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(res)
+
+		f.Close()
+		os.Remove("./test/" + handler.Filename)
+	}
+}
+
+func main() {
+	http.HandleFunc("/upload/", upload)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe Error: ", err)
+	}
+}
